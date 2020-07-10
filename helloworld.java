df6705519b48664dd4a90dfa0159ce01a2653d73
Moinch <43258060+Moinch@users.noreply.github.com>
2020-07-10 15:48:03 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class helloworld {
     public static void main(String[] args) {
-		for (int i = 0; i < 51; i++) {
+		for (int i = 0; i < 1; i++) {
 			System.out.println("Hello1111111111111111");
 		}
 	}
